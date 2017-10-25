2b74764474b5e88103d9dd3e920ffaa7581f986d
Kim Konstantin <james095@yandex.ru>
2017-10-25 13:33:15 +0300

    Added login, signup and other minor changes

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/beardedkorean/Projects/django-tutorial/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
--- mysite/settings.py
+++ mysite/settings.py
@@ -100,6 +100,8 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+LOGIN_REDIRECT_URL = '/'
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.11/topics/i18n/
@@ -119,4 +121,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
